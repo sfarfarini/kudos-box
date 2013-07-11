d67f7f496877de302bbe94091385b1b7b23ab591
Raffaele Cigni <rcigni@byte-code.com>
2013-07-11 19:55:50 +0200

    basic validation

--- client/client.js
+++ client/client.js
@@ -1,26 +1,46 @@
-  Template.kudo_form.from = function () {
+Template.kudo_form.from = function () {
     return Meteor.userId();
-  };
+};
 
-  Template.kudo_form.events({
+Template.kudo_form.events({
     'click button' : function () {
-      // template data, if any, is available in 'this'
-	  var to = $('#to').val();
-	  var reason = $('#reason').val();
-	  var from = Meteor.user().profile.name.toString();
-	  var when = (new Date()).getTime();
-	  
-	  Kudos.insert({ 
-	  	to: to, 
-	  	reason: reason, 
-	  	from: from,
-	  	when: when
-	  });
-	  
-	  return false;
+        // template data, if any, is available in 'this'
+        var to = $('#to').val();
+        var reason = $('#reason').val();
+        var from = Meteor.user().profile.name.toString();
+        var when = new Date();
+
+        if ( to != '' || form != '' ) {
+
+            $('#to').val('');
+            $('#reason').val('');
+
+            Kudos.insert({
+                to: to,
+                reason: reason,
+                from: from,
+                when: when
+            });
+
+        } else {
+
+            alert('Are u making fun of me?');
+
+        }
+
+        return false;
     }
-  });
-  
-  Template.kudo_list.kudos = function () {
+});
+
+Template.kudo_list.kudos = function () {
+
     return Kudos.find({}, {sort: {when: -1}});
-  };
+};
+
+Template.navbar.events({
+    'click a.drop': function() {
+        var one = Kudos.findOne({}, {sort: {when: -1}});
+        Kudos.remove(one._id);
+        return false;
+    }
+});

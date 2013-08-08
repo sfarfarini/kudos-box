40fad9580acd5033ad977b53140817095803d4b2
Samuele Farfarini <sfarfarini@byte-code.com>
2013-08-08 17:45:08 +0200

    added details in showusers

--- client/showUser.js
+++ client/showUser.js
@@ -3,6 +3,14 @@ Template.showUser.user = function() {
     return Users.findOne(Session.get('showUser._id'));
 };
 
+Template.showUser.events({
+
+    'mouseover li.domain' : function() {
+        Session.set('domain.edit', true);
+        console.log(Session.get('domain.edit'));
+    }
+});
+
 Template.kudo_short_given_list.kudos = function() {
     return Kudos.find({ fromId: Session.get('showUser._id') }, { sort: { when: -1 }});
 };
@@ -11,11 +19,8 @@ Template.kudo_short_received_list.kudos = function() {
     return Kudos.find({ toId: Session.get('showUser._id') }, { sort: { when: -1 }});
 };
 
-Template.kudo_short.helpers({
+Template.kudo_short_given.helpers({
 
-    from: function() {
-        return safeName(Users.findOne(this.fromId));
-    },
     to: function() {
         return safeName(Users.findOne(this.toId));
     },
@@ -23,4 +28,36 @@ Template.kudo_short.helpers({
         return moment(this.when).fromNow();
     }
 });
+
+Template.kudo_short_received.helpers({
+
+    from: function() {
+        return safeName(Users.findOne(this.fromId));
+    },
+    prettyWhen: function() {
+        return moment(this.when).fromNow();
+    }
+});
+
+Template.kudo_short_details.helpers({
+
+    likes: function() {
+        var kudo = Kudos.findOne({_id: this._id});
+        return kudo.likes ? kudo.likes.length : 0;
+    },
+    comments: function() {
+        var kudo = Kudos.findOne({_id: this._id});
+        return kudo.comments ? kudo.comments.length : 0;
+    }
+});
+
+Template.kudo_short_details.events({
+
+    'click a.public-link': function(e, t) {
+        e.preventDefault();
+        window.open('http://' + document.location.host + '/share/' + this._id);
+    }
+});
+
+
 
